a5a5710b3ccfaf672e0c82840a2a4d6281e579aa
rsantaeulalia <rodrigo.santaeulalia@gmail.com>
2021-06-27 20:50:03 -0300

    added endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
@@ -12,9 +13,29 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+// post, upload file
+app.post('/api/fileanalyse', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send({"name": file.name, "type": file.type, "size":32574});
+})
